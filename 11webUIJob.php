76395dbd9a469d03fd6dcaa6a84452eba5f0205e
308339@qq.com <>
2017-10-31 21:55:09 +0800

    f32684b65e2d65cf23abb09a392120463d1e3ded---f32684b65e2d65cf23abb09a392120463d1e3ded

--- 11webUIJob.php
+++ 11webUIJob.php
@@ -8,7 +8,7 @@
 	<script type="text/javascript">
 	// js功能部分，作用：对本页面的所有按钮的动作做出定义
 	function ajaxSend(op, clientid, shuifa)
-	{	
+	{
 	var xmlhttp;
 	if (window.XMLHttpRequest)
 	  {// code for IE7+, Firefox, Chrome, Opera, Safari
@@ -28,17 +28,17 @@
 	// 判断按钮的用途
 	if (op == 'clearjob') {
 		// 清除任务队列
-		xmlhttp.open("POST","14phpClearJobList.php",false);
+		xmlhttp.open("POST","./14phpClearJobList.php",false);
 		xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
 		xmlhttp.send();
 	}
-	else 
+	else
 	if (op == 'jobSend') {
 		// 上传新任务
 		controData={'clientid':clientid, 'shuifa':shuifa};
 		jsonStr='jsonStr='+JSON.stringify(controData);
 		// alert(jsonStr);
-		xmlhttp.open("POST","12phpJobUploadTest.php",false);
+		xmlhttp.open("POST","./12phpJobUploadTest.php",false);
 		xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
 		xmlhttp.send(jsonStr);
 	};
@@ -50,13 +50,15 @@
 
 	<br>
 	传感器数据
-	<hr width="350" align="left">
-	<table border="1" width="350">
+	<hr width="400" align="left">
+	<table border="1" width="400">
 		<tr>
 		<td>id</td>
 		<td>温度</td>
 		<td>湿度</td>
+		<td>CO2</td>
 		<td>水阀</td>
+		<td>风扇</td>
 		<td>其他</td>
 		<td>水阀控制</td>
 	</tr>
@@ -69,7 +71,9 @@
 					<td>".$eachRow['id']."</td>
 					<td>".$eachRow['wendu']."</td>
 					<td>".$eachRow['shidu']."</td>
+					<td>".$eachRow['co2']."</td>
 					<td>".$eachRow['shuifa']."</td>
+					<td>".$eachRow['fengshan']."</td>
 					<td>".$eachRow['qita']."</td>
 					<td><button onclick=\"ajaxSend('jobSend',".$eachRow['id'].",'1')\">ON</button>
 						<button onclick=\"ajaxSend('jobSend',".$eachRow['id'].",'0')\">OFF</button></td>
@@ -88,8 +92,8 @@
 		$row =  $dbObj->query("select * from sys_stat where key='jobcount'")->
 				fetchArray(SQLITE3_ASSOC);
 		echo $row["value"];?>
-	<hr width="350" align="left">
-	<table border="1" width="350">
+	<hr width="400" align="left">
+	<table border="1" width="400">
 		<tr>
 		<td>任务ID</td>
 		<td>传感器ID</td>
