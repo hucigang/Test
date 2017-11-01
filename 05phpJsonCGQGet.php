a291ccfd6022e6dacae6f7856fe0567753a812ac
308339@qq.com <>
2017-11-01 08:45:05 +0800

    0---f32684b65e2d65cf23abb09a392120463d1e3ded

--- 05phpJsonCGQGet.php
+++ 05phpJsonCGQGet.php
@@ -3,7 +3,6 @@
 	
 	// PHP SQL JSON TEST
 	// header('content-type:application/json;charset=utf8');
-	header("Access-Control-Allow-Origin: *");
 
 	$DB_NAME	= 'test01.db';
 	$dbObj		= new SQLite3($DB_NAME);
